f82d5cd3242844f9ce16e97c74588de749ad6d42
thdaemon <thxdaemon@gmail.com>
2017-05-05 17:35:23 +0800

    Add interfaces to get all processes portablely

---
+++ os/linux/proc.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "../proc.h"
+
+DIR* dir;
+
+int ueld_os_for_each_process(pid_t* pid)
+{
+	if ((dir = opendir("/proc")) == NULL)
+		return -1;
+
+	return ueld_os_next_process(pid);
+}
+
+int ueld_os_next_process(pid_t* pid)
+{
+	struct dirent* dent;
+
+reread:
+	if ((dent = readdir(dir)) == NULL)
+		return 0;
+
+	pid_t p = atol(dent->d_name);
+	if (p != 0 && p != 1){
+		*pid = p;
+	} else {
+		goto reread;
+	}
+
+	return 1;
+}
+
+int ueld_os_end_each_process()
+{
+	return closedir(dir);
+}
