3f10ad70fdc74ecfd5c4f8e2a17be5898adefb65
thdaemon <thxdaemon@gmail.com>
2017-05-10 20:35:44 +0800

    move umountfs code from reboot.c to os/.../mnt.c so that the portibality is improved

--- reboot.c
+++ reboot.c
@@ -6,22 +6,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <errno.h>
-#include <sys/mount.h>
 #include <sys/wait.h>
-
-#ifndef CONFIG_MANU_GET_MNTINFO
-#include <mntent.h>
-#endif /* CONFIG_MANU_GET_MNTINFO */
 
 #include "os/pw.h"
 #include "os/chvt.h"
 #include "os/proc.h"
+#include "os/mnt.h"
 #include "fileio.h"
 #include "tools.h"
 #include "respawn.h"
@@ -31,14 +26,6 @@
 #ifndef CONFIG_TERM_WAITTIME
 #define CONFIG_TERM_WAITTIME 2
 #endif /* CONFIG_TERM_WAITTIME */
-
-#define MOUNTS "/etc/mtab"
-
-#ifdef LINUX
-#define MOUNTS_LINUX "/proc/self/mounts"
-#else
-#define MOUNTS_LINUX MOUNTS
-#endif /* LINUX */
 
 static void killproc(int signo)
 {
@@ -52,151 +39,6 @@ static void killproc(int signo)
 
 	ueld_os_end_each_process();
 }
-
-static int _ueld_umount(char* fsname, char* dir, char* type)
-{
-	int fd;
-
-	ueld_print("Trying to unmount %s\n", dir);
-
-	if ((fd = open(dir, O_RDONLY)) >= 0) {
-		syncfs(fd);
-		close(fd);
-	}
-
-	if (umount(dir) == 0)
-		return 0;
-
-	if (mount(fsname, dir, type, MS_MGC_VAL|MS_RDONLY|MS_REMOUNT, NULL) < 0) {
-		/* EINVAL means that the filesystem is not mounted. */
-		if (errno != EINVAL) {
-			ueld_print("Re-mount %s failed (%s), system will"
-			           " not poweroff or poweroff unsafely if"
-			           " the problem can not be fixup later.\n",
-			           dir, strerror(errno));
-			return -1;
-		}
-	}
-
-	return 0;
-}
-
-#ifdef CONFIG_MANU_GET_MNTINFO
-#ifndef LINUX
-#error "CONFIG_MANU_GET_MNTINFO only support Linux platform."
-#endif /* LINUX */
-static int ueld_umount(char* info)
-{
-	char *src, *target, *type;
-
-	src = info;
-	target = strchr(src, ' ');
-	*target++ = 0;
-	type = strchr(target, ' ');
-	*type++ = 0;
-
-	return _ueld_umount(src, target, type);
-}
-
-static char* readline(char* buffer, char** next)
-{
-	char* tmp;
-
-	tmp = strchrnul(buffer, '\n');
-
-	if (*tmp == 0)
-		return 0;
-
-	*tmp = 0;
-	*next = tmp + 1;
-
-	return buffer;
-}
-
-static int _umount_all(char* mntinfo, size_t length)
-{
-	int umount_fail_cnt;
-	char *mnts, *p, *next;
-
-	if ((mnts = malloc(length)) == NULL)
-		return -1;
-
-	memcpy(mnts, mntinfo, length);
-
-	umount_fail_cnt = 0;
-	next = mnts;
-	while ((p = readline(next, &next)) != 0) {
-		if (ueld_umount(p) < 0)
-			umount_fail_cnt++;
-	}
-
-	free(mnts);
-	return umount_fail_cnt;
-}
-
-static int umount_all()
-{
-	int fd, umount_fail_cnt, n;
-	off_t length;
-	char* buffer;
-
-	if ((fd = open(MOUNTS_LINUX, O_RDONLY)) < 0) {
-		ueld_print("open: %s", strerror(errno));
-		return -1;
-	}
-
-	if ((length = readnsa(fd, &buffer)) < 0) {
-		ueld_print("read: %s", strerror(errno));
-		return -1;
-	}
-
-	if (realloc(buffer, length + 1) == 0) {
-		ueld_print("realloc mem failed.");
-		return -1;
-	}
-	buffer[length] = 0;
-	close(fd);
-
-	length++;
-
-	n = 0;
-	if ((umount_fail_cnt = _umount_all(buffer, length)) > 0)
-		n = umount_fail_cnt;
-
-	for (int i = 0; i < n; i++) {
-		if ((umount_fail_cnt = _umount_all(buffer, length)) == 0)
-			break;
-	}
-
-	free(buffer);
-
-	return umount_fail_cnt;
-}
-#else
-static int umount_all()
-{
-	int fd;
-	char* buffer;
-	FILE* mnt;
-	struct mntent* ent;
-	unsigned int umount_fail_cnt;
-
-	if ((mnt = mysetmntent(MOUNTS_LINUX, "r", &fd, &buffer)) == NULL)
-		mnt = mysetmntent(MOUNTS, "r", &fd, &buffer);
-
-	if (mnt == NULL)
-		return -1;
-
-	umount_fail_cnt = 0;
-	while ((ent = getmntent(mnt)) != NULL) {
-		if (_ueld_umount(ent->mnt_fsname, ent->mnt_dir, ent->mnt_type) < 0)
-			umount_fail_cnt++;
-	}
-
-	myendmntent(mnt, &fd, &buffer);
-	return umount_fail_cnt;
-}
-#endif /* CONFIG_MANU_GET_MNTINFO */
 
 #ifndef CONFIG_CONSOLE_VT
 #define CONFIG_CONSOLE_VT 1
@@ -245,7 +87,7 @@ int ueld_reboot(int cmd)
 
 	ueld_echo("Unmounting/Re-mounting file systems...");
 
-	if ((umount_all() != 0) && (ueld_readconfiglong("ueld_must_remount_before_poweroff", -1) == 1)) {
+	if ((ueld_os_umount_all() != 0) && (ueld_readconfiglong("ueld_must_remount_before_poweroff", -1) == 1)) {
 		ueld_echo("Remount some filesystems failed, droping to a shell...");
 		char* sh = ueld_readconfig("system_shell");
 		if (!sh)
